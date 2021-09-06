c8022df98e156c724bc9794299dff40f94d50895
raiyan <raiyan5764@gmail.com>
2021-09-06 21:27:14 +0600

    delete added

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ function getNotes() {
 function createDivNotes(titleInput, notesInput) {
   console.log(titleInput, notesInput);
   let col = document.createElement("div");
-  col.className = "col-12 col-md-4 col-lg-4";
+  col.className = "col-12 col-md-4 col-lg-4 main-col";
   col.innerHTML = `
   <div class="card animate__animated animate__fadeInDown main-card" style="width: 100%; height: 100%">
             <div class="card-body">
@@ -46,11 +46,13 @@ function createDivNotes(titleInput, notesInput) {
               <p class="card-text" onclick="editDes()">
                ${notesInput}
               </p>
-              <button class="btn btn-warning" onclick="delNote()">Delete</button>
+              <button class="btn btn-warning btn-delete">Delete</button>
             </div>
           </div>
   `;
   allNotes.appendChild(col);
+  console.log(col);
+  delBtn();
 }
 
 function editTitle() {
@@ -67,10 +69,14 @@ function editDes() {
     card.classList.add("outline");
   });
 }
-
-function delNote() {
-  alert("hello");
-  event.stopPropagation();
+// Delete note
+function delBtn() {
+  let parent = document.querySelector(".all-notes");
+  parent.addEventListener("click", (e) => {
+    if (e.target.classList.contains("btn-delete")) {
+      e.target.parentNode.parentNode.parentNode.remove();
+    }
+  });
 }
 
 // Event tiggers
